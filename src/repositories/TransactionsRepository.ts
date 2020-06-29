af1396f34b66e1544d947f6a4047bc7715a1bff9
joao vitor <joao3klb@hotmail.com>
2020-06-29 20:11:13 -0300

    tarefa bootcamp

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface transaction{
+  title:string;
+  value:number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -15,14 +20,51 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const income = this.transactions.reduce((previous, current ) =>{
+      if(current.type == 'income'){
+        return previous + current.value;
+      }
+      return previous;
+    }, 0);
+
+    const outcome = this.transactions.reduce((previous, current ) =>{
+      if(current.type == 'outcome'){
+        return previous + current.value;
+      }
+      return previous;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+       income,
+       outcome,
+       total
+    }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title , value, type }:transaction ): Transaction {
+    // TOD
+
+    const { total } = this.getBalance();
+
+    if(total < value &&  type == 'outcome'){
+      throw new Error(`Saldo insuficiente, Seu saldo atual e de R$${total}`);
+    }
+    const transaction = new Transaction({
+      title,
+      value,
+      type
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,42 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+
+const transactionsRepository = new TransactionsRepository();
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+
 
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.status(200).json({transactions,balance});
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
+  const { title , value , type }  = request.body;
   try {
     // TODO
+    if(type !== 'income' && type !== 'outcome'){
+      throw new Error('Type invalid');
+    }
+
+    const createTransactionService = new CreateTransactionService(transactionsRepository);
+
+    const transaction = createTransactionService.execute({title,value,type});
+
+    return response.status(200).json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
